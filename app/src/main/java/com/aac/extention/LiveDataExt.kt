8f096f67c08d3ad4f7be7dd2ac2831e4812127c6
TakakiTojo <makuhari109@gmail.com>
2018-11-03 16:41:56 +0900

    refactoring

--- app/src/main/java/com/aac/extention/LiveDataExt.kt
+++
@@ -1,6 +0,0 @@
-package com.aac.extention
-
-import android.arch.lifecycle.LiveDataReactiveStreams
-import org.reactivestreams.Publisher
-
-fun <T> Publisher<T>.toLiveData() = LiveDataReactiveStreams.fromPublisher(this)
